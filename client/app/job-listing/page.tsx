b1ec572218424be769c9a8ef51b48329d6d62d60
blockend <oladayoahmod112@gmail.com>
2024-06-13 19:48:38 +0100

    listing all jobs functionalities

---
+++ client/app/job-listing/page.tsx
@@ -0,0 +1,22 @@
+import AllJobs from "../../components/AllJobs"
+import Footer from "../../components/Footer"
+import JobListing from "../../components/JobListing"
+import MobileNavbar from "../../components/MobileNavbar"
+import Navbar from "../../components/Navbar"
+
+function ListingPage(){
+    return (
+        <main className="wrapper ovh">
+              {/* <div className="preloader"></div> */}
+            <Navbar />
+            <MobileNavbar />
+            <div className="body_content">
+               <JobListing />
+               <AllJobs />
+                <Footer />
+            </div>
+        </main>
+    )
+}
+
+export default ListingPage
