c01396f543d08a853353033ceef25c640a23a7c7
blockend <oladayoahmod112@gmail.com>
2024-07-01 17:53:25 +0100

    integrated paymaster

---
+++ deploy/deploy-paymaster.ts
@@ -0,0 +1,26 @@
+import { deployContract, getWallet, getProvider } from "./utils";
+import * as ethers from "ethers";
+
+export default async function () {
+  const ZKFreelance = await deployContract("Freelance");
+  const ZKFreelanceAddress = await ZKFreelance.getAddress();
+  const paymaster = await deployContract("ZKFreelancePaymaster", [ZKFreelanceAddress]);
+
+  const paymasterAddress = await paymaster.getAddress();
+
+  // Supplying paymaster with ETH
+  console.log("Funding paymaster with ETH...");
+  const wallet = getWallet();
+  await (
+    await wallet.sendTransaction({
+      to: paymasterAddress,
+      value: ethers.parseEther("0.06"),
+    })
+  ).wait();
+
+  const provider = getProvider();
+  const paymasterBalance = await provider.getBalance(paymasterAddress);
+  console.log(`Paymaster ETH balance is now ${paymasterBalance.toString()}`);
+
+  console.log(`Done!`);
+}
